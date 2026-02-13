fee43b21cb77b90c2973284fc51d49006f89527b
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-13 16:40:21 +0530

    feat : implement medicine function

--- src/main/java/com/dev/controller/DashboardFormController.java
+++ src/main/java/com/dev/controller/DashboardFormController.java
@@ -1,5 +1,45 @@
 package com.dev.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+    Stage stage = new Stage();
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"))));
+            stage.show();
+            stage.setTitle("HeloCare - Dashboard");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnInventoryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMedicineOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSalesBillingOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnAction(ActionEvent event) {
+
+    }
 
 }
