7108157a5cf9d30d2790c92d3ff9cdec035787c8
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-14 13:16:45 +0530

    feat : implement sales and billing form.

---
+++ src/main/java/com/dev/controller/SalesAndBillingFormController.java
@@ -0,0 +1,68 @@
+package com.dev.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class SalesAndBillingFormController {
+
+    @FXML
+    private ComboBox<?> cmbPaymentMethod;
+
+    @FXML
+    private TableColumn<?, ?> colDiscountAmount;
+
+    @FXML
+    private TableColumn<?, ?> colPaymentMethod;
+
+    @FXML
+    private TableColumn<?, ?> colSaleId;
+
+    @FXML
+    private TableColumn<?, ?> colTaxAmount;
+
+    @FXML
+    private TableColumn<?, ?> colTotalAmount;
+
+    @FXML
+    private TableColumn<?, ?> colTransactionTotal;
+
+    @FXML
+    private DatePicker txtDate;
+
+    @FXML
+    private TextField txtDiscountAmount;
+
+    @FXML
+    private TextField txtSaleID;
+
+    @FXML
+    private TextField txtTaxAmount;
+
+    @FXML
+    private TextField txtTotalAmount;
+
+    @FXML
+    void btnAddSaleOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteSaleOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchSaleOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateSaleOnAction(ActionEvent event) {
+
+    }
+
+}
