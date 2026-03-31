00489134e1dfe369a22787affbcc9630359911a5
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-04-01 00:15:21 +0530

    feat(config): load database connection settings from properties file

--- src/main/java/com/dev/db/DBConnection.java
+++ src/main/java/com/dev/db/DBConnection.java
@@ -1,15 +1,31 @@
 package com.dev.db;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
     private static DBConnection instance;
     private Connection connection;
 
     private DBConnection() throws SQLException {
-        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pims_smd","root","20031207");
+        try {
+            // Load database properties from file
+            Properties props = new Properties();
+            InputStream input = getClass().getClassLoader().getResourceAsStream("database.properties");
+            props.load(input);
+            
+            // Get connection details from properties
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+            
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            throw new SQLException("Failed to connect to database: " + e.getMessage());
+        }
     }
 
     public static DBConnection getInstance() throws SQLException {
